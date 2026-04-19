707c0f1cd03021cebe7c32548f5d26c99389481d
Caden <cadenf614@gmail.com>
2026-04-19 19:00:19 -0400

    Anilist cache update

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,11 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'seedProfiles.js']),
+  {
+    files: ['generateProfiles.js'],
+    languageOptions: { globals: globals.node },
+  },
   {
     files: ['**/*.{js,jsx}'],
     extends: [
